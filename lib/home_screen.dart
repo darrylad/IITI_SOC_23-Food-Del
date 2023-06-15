15643539350f7684198e3f7521006d24df96ee13
Shivam Sharma <mastichorshivam@gmail.com>
2023-06-15 22:13:52 +0530

    home page ver 1.1.1

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 class StartscreenWidget extends StatelessWidget {
   const StartscreenWidget({super.key});
@@ -49,74 +48,14 @@ class StartscreenWidget extends StatelessWidget {
                   ])),
             )),
           ),
+          const SizedBox(height: 50),
           Positioned(
-              top: 632,
-              left: 159,
-              child: SizedBox(
-                  width: 75,
-                  height: 75,
-                  child: Stack(children: <Widget>[
-                    Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Container(
-                            width: 75,
-                            height: 75,
-                            decoration: const BoxDecoration(
-                              color: Color.fromRGBO(255, 255, 255, 1),
-                              borderRadius:
-                                  BorderRadius.all(Radius.elliptical(75, 75)),
-                            ))),
-                    const Positioned(
-                        top: 37,
-                        left: 25,
-                        child: Divider(
-                            color: Color.fromRGBO(249, 104, 0, 1),
-                            thickness: 5)),
-                    Positioned(
-                        top: 22,
-                        left: 59,
-                        child: Transform.rotate(
-                          angle: -49.08561712407484 * (math.pi / 180),
-                          child: const Divider(
-                              color: Color.fromRGBO(249, 104, 0, 1),
-                              thickness: 5),
-                        )),
-                    Positioned(
-                        top: 52,
-                        left: 59,
-                        child: Transform.rotate(
-                          angle: 49.08561712407484 * (math.pi / 180),
-                          child: const Divider(
-                              color: Color.fromRGBO(249, 104, 0, 1),
-                              thickness: 5),
-                        )),
-                  ]))),
-          Positioned(
-              top: 613,
-              left: 91,
-              child: Container(
-                  width: 179,
-                  height: 54,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(95),
-                      topRight: Radius.circular(95),
-                      bottomLeft: Radius.circular(95),
-                      bottomRight: Radius.circular(95),
-                    ),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Color.fromRGBO(0, 0, 0, 0.20000000298023224),
-                          offset: Offset(0, 0),
-                          blurRadius: 35)
-                    ],
-                    color: const Color.fromRGBO(255, 255, 255, 1),
-                    border: Border.all(
-                      color: const Color.fromRGBO(155, 65, 0, 1),
-                      width: 3,
-                    ),
-                  )))
+              top: 630,
+              left: 144,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Get Started"),
+              )),
         ]));
   }
 }
