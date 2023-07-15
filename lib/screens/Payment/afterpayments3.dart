daea6de941d62f34cf358a74096dbeeafaaf9697
Shivam Sharma <mastichorshivam@gmail.com>
2023-07-15 09:54:35 +0530

    darryl ke liye

--- lib/screens/Payment/afterpayments3.dart
+++ lib/screens/Payment/afterpayments3.dart
@@ -1,10 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+// import 'dart:ui';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Afterpayments3 extends StatelessWidget {
-  const Afterpayments3({super.key});
+class DeliveredScreen extends StatelessWidget {
+  final String deliveryAgentName = "Abhinav Tiwari";
+  final String deliveryAgentPhone = "9876543210";
+
+  const DeliveredScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double swidth = MediaQuery.of(context).size.width;
+    double sheight = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        width: swidth,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/buttons/greendarryl.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              top: 80,
+              child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset('assets/buttons/check.png')),
+            ),
+            Positioned.fill(
+              bottom: 120, // distance from bottom of screen
+
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: swidth * 0.85,
+                  height: 230,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(23),
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        // first item in stack, delivery agent name
+                        top: 25,
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            deliveryAgentName,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 30,
+                              color: Colors.black.withOpacity(0.8),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                          top: 85,
+                          child: InkWell(
+                            onTap: () async {
+                              final Uri url = Uri(
+                                scheme: 'tel',
+                                path: deliveryAgentPhone,
+                              );
+                              if (await canLaunchUrl(url)) {
+                                await launchUrl(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Container(
+                                width: swidth * 0.5,
+                                height: 39,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(9),
+                                  color: Colors.black.withOpacity(0.1),
+                                ),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SvgPicture.asset(
+                                        'assets/buttons/phone.png',
+                                        height: 25,
+                                        width: 25,
+                                      ),
+                                      SizedBox(width: swidth * 0.02),
+                                      Text(
+                                        deliveryAgentPhone,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Inter',
+                                          fontSize: 19,
+                                          color:
+                                              const Color.fromRGBO(0, 46, 45, 1)
+                                                  .withOpacity(0.8),
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )),
+                      Positioned.fill(
+                        bottom: 40,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            "Delivered",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 25,
+                              color: Colors.black.withOpacity(0.6),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
